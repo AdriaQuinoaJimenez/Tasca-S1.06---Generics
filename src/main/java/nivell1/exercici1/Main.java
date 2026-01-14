576aafe6be99f5e1605c3996e291ff0e4909f1ce
Adri Quiñoa <adriaquinoajimenez@gmail.com>
2026-01-14 12:45:09 +0100

    feat: exercici1 level1 completed without using generics and casting objects to string

--- src/main/java/nivell1/exercici1/Main.java
+++ src/main/java/nivell1/exercici1/Main.java
@@ -3,5 +3,10 @@ package nivell1.exercici1;
 public class Main {
     public static void main(String[] args) {
 
+        NoGenericMethods noGenericMethods1 = new NoGenericMethods("Good Morning" ,"Good Afternoon", "Good Night");
+        NoGenericMethods noGenericMethods2 = new NoGenericMethods("Good Night", "Good Morning", "Good Afternoon");
+
+        System.out.println("noGenericMethods1: " + (String) noGenericMethods1.getObj1()+ " | "+ noGenericMethods1.getObj2() + " | " + noGenericMethods1.getObj3());
+        System.out.println("noGenericMethods2: " + (String) noGenericMethods2.getObj1()+ " | "+ noGenericMethods2.getObj2() + " | " + noGenericMethods2.getObj3());
     }
 }
--- src/main/java/nivell2/exercici1/Main.java
+++ src/main/java/nivell2/exercici1/Main.java
@@ -2,6 +2,6 @@ package nivell2.exercici1;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
