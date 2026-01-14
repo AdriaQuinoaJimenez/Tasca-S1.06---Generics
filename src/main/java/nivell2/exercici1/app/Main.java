7f8e75111d6f047b500846b0212d4e0d5f2eb6dd
Adri Quiñoa <adriaquinoajimenez@gmail.com>
2026-01-14 13:42:07 +0100

    feat: adding lines to use varargs

--- src/main/java/nivell2/exercici1/app/Main.java
+++ src/main/java/nivell2/exercici1/app/Main.java
@@ -5,6 +5,8 @@ import nivell2.exercici1.model.Person;
 
 public class Main {
     public static void main(String[] args) {
+
+         /*EXERCICI 1 Y EXERCICI 2 LEVEL 2*/
         Person person = new Person("Juan", "Rodriguez", 23);
         Person person2 = new Person("Mario", "Ortiz", 81);
         Person person3 = new Person("Paco", "Martinez", 44);
@@ -18,5 +20,9 @@ public class Main {
         GenericMethods.printElements(word, number, person, person2, person3, person4);
         System.out.println("\n");
         GenericMethods.printElements(word, person3, number, number2, number3);
+        System.out.println("\n");
+        GenericMethods.printElements(word, person4, number);
+        System.out.println("\n");
+        GenericMethods.printElements(word, person4);
     }
 }
