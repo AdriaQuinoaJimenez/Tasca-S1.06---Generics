434f664dfd709142de96336fe2d5106be28afe81
Adri Quiñoa <adriaquinoajimenez@gmail.com>
2026-01-20 11:13:32 +0100

    fix: lvl 1 exercise 1 fixed adding forced cast and changing variable names

--- src/main/java/nivell1/exercici1/app/Main.java
+++ src/main/java/nivell1/exercici1/app/Main.java
@@ -7,8 +7,15 @@ public class Main {
 
         NoGenericMethods noGenericMethods1 = new NoGenericMethods("Good Morning" ,"Good Afternoon", "Good Night");
         NoGenericMethods noGenericMethods2 = new NoGenericMethods("Good Night", "Good Morning", "Good Afternoon");
+        String objectRecovered;
+        int objectLength;
 
         System.out.println("noGenericMethods1: " + (String) noGenericMethods1.getObj1()+ " | "+ noGenericMethods1.getObj2() + " | " + noGenericMethods1.getObj3());
         System.out.println("noGenericMethods2: " + (String) noGenericMethods2.getObj1()+ " | "+ noGenericMethods2.getObj2() + " | " + noGenericMethods2.getObj3());
+
+        objectRecovered = ((String) noGenericMethods1.getObj2());
+        objectLength = objectRecovered.length();
+
+        System.out.println("noGenericMethods1 length: " + objectLength);
     }
 }
--- src/main/java/nivell2/exercici1/model/GenericMethods.java
+++ src/main/java/nivell2/exercici1/model/GenericMethods.java
@@ -2,10 +2,10 @@ package nivell2.exercici1.model;
 
 public class GenericMethods {
 
-    public static <S, V> void printElements(String string, S s, V... v) {
-        System.out.println("1st Element: " + string + " | 2nd Element: " + s);
+    public static <S, V> void printElements(String firstElement, S secondElement, V... varArgs) {
+        System.out.println("1st Element: " + firstElement + " | 2nd Element: " + secondElement);
         System.out.println("3rd Elements: ");
-        for (V v2 : v) {
+        for (V v2 : varArgs) {
             System.out.print(v2 + " | ");
         }
     }
